63a50f9e1a9bdd4276758ece6c1d54a8715c533e
Michele Bucca <michele.bucca@gmail.com>
2023-03-19 09:07:14 +0100

    remove build-cockpit-livecd.sh

--- build-cockpit-livecd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-lb config --architecture amd64 --binary-image iso-hybrid \
-  --updates true --security true --uefi-secure-boot true \
-  --apt-indices false --firmware-chroot true --backports true \
-  --archive-areas "main,contrib,non-free"
